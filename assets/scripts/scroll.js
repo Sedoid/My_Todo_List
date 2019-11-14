246d63750b7623a7e6ea2a24b5e58e87119eb82c
Sedoid <karlsedoide@gmail.com>
2019-11-14 13:23:44 +0100

    added the design of the add Task button

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,6 +1,8 @@
 
 let slider = document.querySelector('.section'),
     container = document.querySelector('#container'),
+    set = document.querySelector('.create')
+    cancel = document.querySelector('.cancel')
     nav = document.querySelectorAll('ul a'),
     add = document.querySelector('#btn'),
     slider_width = slider.scrollWidth;
@@ -14,18 +16,18 @@ window.onclick  = function(event){
     if((event.target == nav[0] || this.event.target.parentNode == nav[0] || container.scrollLeft < 200))
       {      
   
-          nav[1].classList.remove('active');
-          nav[2].classList.remove('active');
-           nav[0].classList.add('active');
+          nav[1].classList.remove('activee');
+          nav[2].classList.remove('activee');
+           nav[0].classList.add('activee');
 
            container.scrollTo({top:0,left:0,behavior:"smooth"});
       } 
 
     if((event.target == nav[1] || this.event.target.parentNode == nav[1]))
       {       
-          nav[0].classList.remove('active');
-          nav[2].classList.remove('active');
-          nav[1].classList.add('active');
+          nav[0].classList.remove('activee');
+          nav[2].classList.remove('activee');
+          nav[1].classList.add('activee');
 
           container.scrollTo({top:0,left:blog,behavior:"smooth"});
 
@@ -34,9 +36,9 @@ window.onclick  = function(event){
       if((event.target == nav[2] || this.event.target.parentNode == nav[2]))
       {
         
-        nav[0].classList.remove('active');
-        nav[1].classList.remove('active');
-        nav[2].classList.add('active');
+        nav[0].classList.remove('activee');
+        nav[1].classList.remove('activee');
+        nav[2].classList.add('activee');
 
         container.scrollTo({top:0,left:blog*2,behavior:"smooth"});
 
@@ -47,13 +49,15 @@ window.onclick  = function(event){
         let view = document.querySelector('.add');
 
           view.classList.add('add_item');
+          view.classList.remove('slideOutDown');
           view.classList.add('animated');
           view.classList.add('slideInUp');
 
           setTimeout(function(){
+           // alert('here')
             document.querySelector('header ul').style.display = "none";
           
-          },500);
+         },540);
            
       }
     
@@ -67,9 +71,9 @@ container.addEventListener('scroll',function(){
   if(this.scrollLeft < blog)
   {      
       console.log('hello');
-      nav[1].classList.remove('active');
-      nav[2].classList.remove('active');
-       nav[0].classList.add('active');
+      nav[1].classList.remove('activee');
+      nav[2].classList.remove('activee');
+       nav[0].classList.add('activee');
       
      //  container.scrollTo({top:0,left:0,behavior:"smooth"});
   } 
@@ -77,9 +81,9 @@ container.addEventListener('scroll',function(){
 if(this.scrollLeft >= blog && this.scrollLeft<blog*2)
   {       
     console.log('hola')
-      nav[0].classList.remove('active');
-      nav[2].classList.remove('active');
-      nav[1].classList.add('active');
+      nav[0].classList.remove('activee');
+      nav[2].classList.remove('activee');
+      nav[1].classList.add('activee');
   
   //    this.scrollTo({top:0,left:411,behavior:"smooth"});
   } 
@@ -87,12 +91,29 @@ if(this.scrollLeft >= blog && this.scrollLeft<blog*2)
   if(this.scrollLeft == blog*2)
   {  
     console.log('hiya')
-    nav[0].classList.remove('active');
-    nav[1].classList.remove('active');
-    nav[2].classList.add('active');
+    nav[0].classList.remove('activee');
+    nav[1].classList.remove('activee');
+    nav[2].classList.add('activee');
 
   //  container.scrollTo({top:0,left:blog*2,behavior:"smooth"});
 
   } 
 
 }) 
+
+set.addEventListener('click', slideDown)
+cancel.addEventListener('click', slideDown)
+
+function slideDown(){
+  let view = document.querySelector('.add');
+
+  view.classList.add('add_item');
+  view.classList.remove('slideInUp');
+  view.classList.add('animated');
+  view.classList.add('slideOutDown');
+
+  setTimeout(function(){
+    document.querySelector('header ul').style.display = "inline-block";
+    document.querySelector('#btn').style.display = "inline-block";
+  },150);
+}
